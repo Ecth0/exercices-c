4dfaf815fc5d3b3c3326c647966a14f9f52b09d8
Enguerrand <enguerrand.le-tourneur-hugon@oteria.fr>
2026-02-09 12:01:56 +0100

    Exos 1 à 3 Genepy

---
+++ 42.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a=42;
+    printf("%d",a);
+    return 0;
+}
---
+++ nb_sec.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    long produit;
+    produit=60*60*24*365;
+    printf("%ld\n",produit );   
+    return 0;
+ 
+}
