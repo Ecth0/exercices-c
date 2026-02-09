9d43714d989c5428a7e7f2219e5f2305e090b9f3
Enguerrand <enguerrand.le-tourneur-hugon@oteria.fr>
2026-02-09 12:30:23 +0100

    Ajout knock

---
+++ knock.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+int main(void)
+{
+    float premier=8958937768937;
+    float deuxieme=2851718461558;
+    float div=premier/deuxieme;
+    printf("%.15f",div);
+    return 0;
+}
