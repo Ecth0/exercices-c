02d81aa5ef5eb825638a0445810c7b24adf35177
Enguerrand <enguerrand.le-tourneur-hugon@oteria.fr>
2026-02-10 09:40:51 +0100

    correction operators

--- operators.c
+++ operators.c
@@ -1,9 +1,10 @@
 #include <stdio.h> 
 int main(void)
 {
-    float premier=8958937768937;
-    float deuxieme=2851718461558;
-    float div=premier/deuxieme;
+    double premier=8958937768937;
+    double deuxieme=2851718461558;
+    double div;
+    div=premier/deuxieme;
     printf("%.15f",div);
     return 0;
 }
