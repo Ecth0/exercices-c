a974afd6c7e260eb7a3153004747e12366b6c88f
Enguerrand Le Tourneur Hugon <enguerrand.le-tourneur-hugon@oteria.fr>
2026-03-09 12:15:32 +0100

    Début pngheader

---
+++ new/struc.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+struct png {
+    char magic[4];
+    char nbytes;
+    char version;
+    char abi;
+};
+
+
+int main(void) {
+    struct png mypngfile;
+
+    int file = open("9mars",O_RDONLY);
+
+    read(file, &mypngfile, sizeof(mypngfile));
+
+    /*assert(mypngfile.magic[0]== '%o');*/
+    assert(mypngfile.magic[1] == 'P');
+    assert(mypngfile.magic[2] == 'N');
+    assert(mypngfile.magic[3] == 'G');
+
+    /*if (mypngfile.nbytes == 1)
+        printf("Number of bytes: 32 bits.\n");
+    else if (mypngfile.nbytes == 2)
+        printf("Number of bytes: 64 bits.\n");
+    else
+        printf("Number of bytes: ???\n");*/
+    close(file);
+}
+
